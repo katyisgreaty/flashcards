f5ee76786cc264047fd3564049ea1d4b226fb94f
Katy Daviscourt and Matt Caswell <katy@>
2017-01-10 14:05:40 -0800

    finalize flashcard hide and show

--- js/scripts.js
+++ js/scripts.js
@@ -35,5 +35,76 @@ $(document).ready(function() {
     $("#funct").hide();
   });
 
+  $(".method").click(function() {
+    $("#method").show();
+    $(".method").hide();
+  });
+  $("#method").click(function() {
+    $(".method").show();
+    $("#method").hide();
+  });
+
+  $(".argument").click(function() {
+    $("#argument").show();
+    $(".argument").hide();
+  });
+  $("#argument").click(function() {
+    $(".argument").show();
+    $("#argument").hide();
+  });
+
+  $(".parameter").click(function() {
+    $("#parameter").show();
+    $(".parameter").hide();
+  });
+  $("#parameter").click(function() {
+    $(".parameter").show();
+    $("#parameter").hide();
+  });
+
+  $(".chain-method").click(function() {
+    $("#chain-method").show();
+    $(".chain-method").hide();
+  });
+  $("#chain-method").click(function() {
+    $(".chain-method").show();
+    $("#chain-method").hide();
+  });
+
+  $(".return").click(function() {
+    $("#return").show();
+    $(".return").hide();
+  });
+  $("#return").click(function() {
+    $(".return").show();
+    $("#return").hide();
+  });
+
+  $(".string").click(function() {
+    $("#string").show();
+    $(".string").hide();
+  });
+  $("#string").click(function() {
+    $(".string").show();
+    $("#string").hide();
+  });
+
+  $(".boolean").click(function() {
+    $("#boolean").show();
+    $(".boolean").hide();
+  });
+  $("#boolean").click(function() {
+    $(".boolean").show();
+    $("#boolean").hide();
+  });
+
+  $(".undefined").click(function() {
+    $("#undefined").show();
+    $(".undefined").hide();
+  });
+  $("#undefined").click(function() {
+    $(".undefined").show();
+    $("#undefined").hide();
+  });
 
 });
