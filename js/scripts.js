d59177e7a51c5eefc8733661060304c677253d97
Katy Daviscourt and Matt Caswell <matt@>
2017-01-10 13:41:34 -0800

    include hide and show for operator flashcard

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".operator").click(function() {
+    $("#operator").show();
+    $(".operator").hide();
+  });
+  $("#operator").click(function() {
+    $(".operator").show();
+    $("#operator").hide();
+  });
+
+});
