7439ba1c1fe338a8748fc803b5aebfe05ce2477c
Katy Daviscourt and Matt Caswell <katy@>
2017-01-10 13:56:58 -0800

    add show/hide for some of our terms

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,32 @@ $(document).ready(function() {
     $("#operator").hide();
   });
 
+  $(".variable").click(function() {
+    $("#variable").show();
+    $(".variable").hide();
+  });
+  $("#variable").click(function() {
+    $(".variable").show();
+    $("#variable").hide();
+  });
+
+  $(".var-name").click(function() {
+    $("#var-name").show();
+    $(".var-name").hide();
+  });
+  $("#var-name").click(function() {
+    $(".var-name").show();
+    $("#var-name").hide();
+  });
+
+  $(".funct").click(function() {
+    $("#funct").show();
+    $(".funct").hide();
+  });
+  $("#funct").click(function() {
+    $(".funct").show();
+    $("#funct").hide();
+  });
+
+
 });
